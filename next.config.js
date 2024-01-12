a30223940f62d40f2e5193596d4971f9cdee181f
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-01-12 12:39:31 -0800

    build: update config files and packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
