36a238a61964a7287acec7a1694086b70b6551d2
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-01-12 12:42:54 -0800

    feat: PageMetadata component

---
+++ src/components/PageMetadata.tsx
@@ -0,0 +1,66 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+import { SITE_URL } from '@/lib/constants'
+import type { Meta, NameMeta, PropMeta } from '@/lib/types'
+
+type PageMetadataProps = {
+  title: string
+  description: string
+  image?: string
+  canonicalUrl?: string
+}
+
+export const PageMetadata = ({
+  description,
+  title,
+  image,
+}: PageMetadataProps) => {
+  const { asPath } = useRouter()
+
+  const desc = description || 'Ethereal Forest - PDX DAO'
+  const siteTitle = 'Ethereal Forest'
+  const fullTitle = `${title} | ${siteTitle}`
+  const origin = process.env.NEXT_PUBLIC_SITE_URL || SITE_URL // TODO: Remove .env var usage after launch
+
+  // Remove any query params (?) or hash links (#)
+  const path = asPath.replace(/[\?\#].*/, '')
+
+  /* e.g. set ethereum.org/about/ to ethereum.org/en/about/ */
+  const url = new URL(path, origin).href
+
+  /* Set fallback ogImage based on path */
+  const ogImage = image || 'hero.png'
+
+  const ogImageUrl = new URL(ogImage, origin).href
+  const metadata: Meta[] = [
+    { name: `description`, content: desc },
+    { name: `image`, content: ogImageUrl },
+    { property: `og:title`, content: fullTitle },
+    { property: `og:description`, content: desc },
+    { property: `og:type`, content: `website` },
+    { name: `twitter:card`, content: `summary_large_image` },
+    { name: `twitter:creator`, content: siteTitle },
+    { name: `twitter:site`, content: siteTitle },
+    { name: `twitter:title`, content: fullTitle },
+    { name: `twitter:description`, content: desc },
+    { name: `twitter:image`, content: ogImageUrl },
+    { property: `og:url`, content: url },
+    { property: `og:image`, content: ogImageUrl },
+    { property: `og:site_name`, content: siteTitle },
+    { name: `docsearch:description`, content: desc },
+  ]
+
+  return (
+    <Head>
+      <title>{fullTitle}</title>
+      {metadata.map((data) => (
+        <meta
+          key={(data as NameMeta).name || (data as PropMeta).property}
+          {...data}
+        />
+      ))}
+      <link rel="canonical" key={url} href={url} />
+    </Head>
+  )
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+// Page metadata
+export type NameMeta = {
+  name: string
+  content: string
+}
+
+export type PropMeta = {
+  property: string
+  content: string
+}
+
+export type Meta = NameMeta | PropMeta
+
