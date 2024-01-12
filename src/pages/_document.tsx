0cf089abe46d3621b3c05f6fd3d531abda637a75
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-01-12 12:44:01 -0800

    refactor: _app and _document page components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ import theme from '../theme'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.cdnfonts.com/css/br-shape" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
