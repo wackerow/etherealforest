b961207c7b2b2393e3a7f90d5639f7db26ee2376
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-01-21 20:47:36 -0800

    refactor: update Nav with new design/links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,6 +10,8 @@ import {
 } from "@chakra-ui/react"
 import { SunIcon, MoonIcon } from "@chakra-ui/icons"
 import { NavLink } from "@/lib/types"
+import { motion, useScroll, useTransform } from "framer-motion"
+import { useEffect, useState } from "react"
 
 export type NavProps = FlexProps & {
   items: NavLink[]
@@ -18,8 +20,29 @@ export const Nav = ({ items, ...props }: NavProps) => {
   const { toggleColorMode } = useColorMode()
   const icon = useColorModeValue(<MoonIcon />, <SunIcon />)
   const insetInlineEnd = useToken("space", "4")
+
+  const { scrollY } = useScroll()
+  const [hidden, setHidden] = useState(true)
+  useEffect(() => {
+    addEventListener(
+      "scroll",
+      () => {
+        setHidden(scrollY.get() === 0)
+      },
+      { passive: true }
+    )
+    // remove event listener on return
+    return () =>
+      removeEventListener("scroll", () => {
+        setHidden(scrollY.get() === 0)
+      })
+  }, [scrollY])
+
+  const titleOpacity = useTransform(scrollY, [72, 96], [0, 1])
+
   return (
     <Box
+      as="nav"
       w="100vw"
       position="sticky"
       top="0"
@@ -35,30 +58,34 @@ export const Nav = ({ items, ...props }: NavProps) => {
       fontFamily="body"
     >
       <Flex
-        justify="center"
-        alignItems="center"
-        gap="8"
-        p="4"
         position="sticky"
         top="0"
+        justify="space-between"
+        alignItems="center"
         maxW="container.lg"
         mx="auto"
         {...props}
       >
-        {items.map(({ name, href }) => (
-          <Link key={href} href={href}>
-            {name}
+        <motion.div style={{ opacity: titleOpacity }}>
+          <Link href="/" color="body" display={hidden ? "none" : "block"}>
+            Ethereal Forest
           </Link>
-        ))}
-        <IconButton
-          position="absolute"
-          style={{ insetInlineEnd }}
-          w="fit-content"
-          icon={icon}
-          variant="ghost"
-          onClick={toggleColorMode}
-          aria-label="Toggle color mode"
-        />
+        </motion.div>
+        <Flex alignItems="center" gap="8" p="4">
+          {items.map(({ name, href }) => (
+            <Link key={href} href={href} color="body">
+              {name}
+            </Link>
+          ))}
+          <IconButton
+            style={{ insetInlineEnd }}
+            w="fit-content"
+            icon={icon}
+            variant="ghost"
+            onClick={toggleColorMode}
+            aria-label="Toggle color mode"
+          />
+        </Flex>
       </Flex>
     </Box>
   )
