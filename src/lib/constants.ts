29661908d7db123c4c5e853440ab087828c1815f
christy-pdx <107286087+christy-pdx@users.noreply.github.com>
2024-01-29 13:51:27 -0800

    update nav links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,9 @@ import { NavLink } from "./types"
 export const SITE_URL = "https://etherealforest.eth.limo"
 
 export const NAV_LINKS: NavLink[] = [
-  { name: "Docs", href: "https://pdx-dao.gitbook.io/pdx-dao-docs/" },
+  { name: "Blog", href: "#blog" },
+  { name: "Docs", href: "#docs" },
+  { name: "MoP", href: "#mop" },
   { name: "Newsletter", href: "#newsletter" },
   // { name: "ILL", href: "#ill" },
 ]
